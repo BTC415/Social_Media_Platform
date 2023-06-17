8f0f5806a5cd0f3331a5494be95f2646da0a7ea5
Topcoder415 <alenschmitt415@gmail.com>
2023-06-17 14:51:33 +0530

    new path added

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-// app.use(express.static(path.join("/client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
